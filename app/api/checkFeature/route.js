bd52f7ed6dcd6701f4091e31681580b47f2b4e23
pawelbi1990 <114158452+pawelbi1990@users.noreply.github.com>
2023-12-29 10:36:55 +0100

    m

--- app/api/checkFeature/route.js
+++ app/api/checkFeature/route.js
@@ -1,14 +1,15 @@
 import { Pool } from 'pg';
 import { NextResponse } from 'next/server';
-import { host, user, database, password, port } from '../../__credentials/credentials.json';
 
-const pool = new Pool({
-    host: host,
-    user: user,
-    database: database,
-    password: password,
-    port: port
-});
+
+export const pool = new Pool({
+    host: process.env.DATABASE_HOST_NAME,
+    user: process.env.DATABASE_USER_NAME,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT
+
+})
 
 
 
