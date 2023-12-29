b8a35a6ef795600d85bd66f671a5ebbb3e12db59
pawelbi1990 <114158452+pawelbi1990@users.noreply.github.com>
2023-12-29 10:32:20 +0100

    m

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Logo from '../public/logo.png'
 import Image from 'next/image'
-import {HiServer} from 'react-icons/hi'
-import {FaSearchPlus} from 'react-icons/fa'
-import {SiPwa} from 'react-icons/si'
-import {PiStack} from 'react-icons/pi'
-import {GiFingerPrint} from 'react-icons/gi'
-import {BsLightbulb} from 'react-icons/bs'
+
+
 
 
 const Header = (props) => {
